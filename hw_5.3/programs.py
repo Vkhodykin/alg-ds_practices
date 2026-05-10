1fc5757c671917528a4d76a19e97a064962fee4d
Vkhodykin <victor.khodykin@gmail.com>
2026-05-10 19:59:23 +0300

    Добавил решение второй задачи в programs.py

--- hw_5.3/programs.py
+++ hw_5.3/programs.py
@@ -20,3 +20,23 @@ def max_in_range(arr: list[int], start: int, end: int) -> tuple[int, int, int]:
 
 # O(n) = 1 + 1 + n * (1 + 1 + 1 + 1 + 1) + 1 + 1 + 1 = 2 + n * 5 + 3 = 5 + n * 5 = n * 5 = n -> O(n)
 
+
+# Задача №2
+def rotate_and_reverse(arr: list[int], k: int) -> list[int]:
+    if not arr:
+        return []
+
+    k = k % len(arr)   # нормализуем k на случай, если оно больше длины списка
+
+    if k > 0:
+        cyclic_shift = arr[-k:] + arr[:-k]  # циклический сдвиг вправо, берем последние k-элементы и переносим в начало, остальные элементы сдвигаются
+    else:
+        cyclic_shift = arr[:] # список копируется
+
+    reversed_result = cyclic_shift[::-1]    # инверсия всего списка
+
+    return reversed_result
+
+# O(n) = 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 = 12 -> O(1)
+
+

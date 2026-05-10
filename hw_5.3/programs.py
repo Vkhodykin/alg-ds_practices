3a4c08bd2540d2f747882f706a73f1f99a252b79
Vkhodykin <victor.khodykin@gmail.com>
2026-05-10 21:22:40 +0300

    Добавил решение третей задачи в programs.py

--- hw_5.3/programs.py
+++ hw_5.3/programs.py
@@ -40,3 +40,30 @@ def rotate_and_reverse(arr: list[int], k: int) -> list[int]:
 # O(n) = 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 = 12 -> O(1)
 
 
+# Задача №3
+def reverse_even_elements(arr: list[int]) -> list[int]:
+    if not arr:
+        return []
+
+    even_elements = []
+
+    for element in arr:
+        if element % 2 == 0:  # Находим все четные элементы
+            even_elements.append(element)
+
+    reversed_even_elements = even_elements[::-1] # Разворачиваем четные элементы
+
+    new_arr = []
+    even_elements_index = 0
+
+    for element in arr:
+        if element % 2 == 0:
+            new_arr.append(reversed_even_elements[even_elements_index]) # Добавляем следующий четный элемент из развернутого списка
+            even_elements_index += 1
+        else:
+            new_arr.append(element) # Оставляем нечетный элемент на месте
+
+    return new_arr
+
+# O(n) = n * (1 + 1 + 1 + 1) + 1 + 1 + 1 + n * (1 + 1 + 1 + 1 + 1 + 1 + 1) = n * 4 + 3 + n * 7 = n * 4 + n * 7 =
+#      = n -> O(n)
